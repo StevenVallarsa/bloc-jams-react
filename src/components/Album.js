b325627b874520e78dc8565a35406822b2f0fdd6
StevenVallarsa <orionsjv@gmail.com>
2019-02-11 19:00:54 -0500

    Added track information for album

--- src/components/Album.js
+++ src/components/Album.js
@@ -32,7 +32,17 @@ class Album extends Component {
                         <col id="song-title-column" />
                         <col id="song-duration-column" />
                     </colgroup>
+
                     <tbody>
+                        {this.state.album.songs.map( (song, index) => 
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{song.title}</td>
+                                <td>{song.duration}</td>
+                            </tr>
+                            
+                        ) }
+                        
                     </tbody>
                 </table>
 
