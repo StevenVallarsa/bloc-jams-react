fdeed10641a17d18e32fb801418ab9cab3eed0b7
StevenVallarsa <orionsjv@gmail.com>
2019-02-12 19:17:44 -0500

    checkpoint 2760 updates

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
     constructor(props) {
@@ -51,6 +52,15 @@ class Album extends Component {
             this.setState({ iconStyle: "icon ion-md-pause" })
         }
     }
+
+    handlePrevClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.max(0, currentIndex - 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+    }
+
     handleMouseEnter(index) {
         this.setState({ hoverTrack: index})
         if (this.state.trackPlaying === index) {
@@ -99,7 +109,12 @@ class Album extends Component {
                         
                     </tbody>
                 </table>
-
+                <PlayerBar 
+                    isPlaying={this.state.isPlaying} 
+                    currentSong={this.state.currentSong} 
+                    handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+                    handlePrevClick={() => this.handlePrevClick()} 
+                />
             </section>
         );
     }
